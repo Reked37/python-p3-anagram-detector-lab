bb12c9ff00110c3d3e8ef10e1104a02c3301b81d
Derek Truesdale <dtruesdale37@gmail.com>
2023-07-20 16:34:38 -0500

    lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
-# your code goes here!
+import ipdb
+
+class Anagram:
+    def __init__ (self, word="the list"):
+        self.word=word
+        
+
+    def match (self, alist):
+        characters=[*self.word]
+        new_list=[]
+        for word in alist:
+            if sorted(characters) == sorted([*word]):
+                new_list.append(word)
+        return new_list
+
+        # ipdb.set_trace()
+        
+            
+        
+            
+            
+                
